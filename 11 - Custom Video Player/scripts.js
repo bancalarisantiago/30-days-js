47caef39d56a7ee46712665c57f29f6baa2e57e9
Santiago Bancalari <bancalarisantiago@gmail.com>
2022-06-06 16:17:45 -0300

    Toggle play button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,7 +2,14 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progess = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
